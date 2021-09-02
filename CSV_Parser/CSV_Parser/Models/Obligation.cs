a9da4fbc5da407da01405b1099cb44e840677c4c
Joeaverbeck <89948713+Joeaverbeck@users.noreply.github.com>
2021-09-02 15:31:23 -0500

    Interview code
    CSV_Parser

---
+++ CSV_Parser/CSV_Parser/Models/Obligation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSV_Parser.Models
+{
+    public class Obligations
+    {
+        public int TransID { get; set; }
+        public string Order { get; set; }
+        public string TransactionType { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ CSV_Parser/CSV_Parser/Models/TotalPayment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSV_Parser.Models
+{
+    public class TotalPayment
+    {
+        public string Order { get; set; }
+        public string Name { get; set; }
+        public decimal TotalOb { get; set; }
+        public decimal Paid { get; set; }
+        public decimal Owed { get; set; }
+    }
+}
